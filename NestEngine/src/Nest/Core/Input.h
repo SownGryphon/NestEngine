849342cc712a60708a1ecb96bd7e8e7d7546bf6a
SownGryphon <zhenyastratford@gmail.com>
2024-04-27 20:28:43 +1000

    Added mouse input

--- NestEngine/src/Nest/Core/Input.h
+++ NestEngine/src/Nest/Core/Input.h
@@ -3,6 +3,8 @@
 #include "Nest/Core/Core.h"
 #include "Nest/Core/KeyCodes.h"
 
+#include "geometry/Vector2.h"
+
 namespace Nest
 {
 	class Input
@@ -15,8 +17,18 @@ namespace Nest
 
 		inline static bool IsKeyPressed(int keyCode) { return s_instance->isKeyPressedImpl(keyCode); }
 
+		inline static bool IsMouseButtonDown(int button) { return s_instance->isMouseButtonDownImpl(button); }
+		inline static float GetMouseX() { return s_instance->getMouseXImpl(); }
+		inline static float GetMouseY() { return s_instance->getMouseYImpl(); }
+		inline static chcl::Vector2<float> GetMousePos() { return s_instance->getMousePosImpl(); }
+
 	private:
 		virtual bool isKeyPressedImpl(int keyCode) = 0;
+		
+		virtual bool isMouseButtonDownImpl(int button) = 0;
+		virtual float getMouseXImpl() = 0;
+		virtual float getMouseYImpl() = 0;
+		virtual chcl::Vector2<float> getMousePosImpl() = 0;
 
 		static Scope<Input> s_instance;
 	};
