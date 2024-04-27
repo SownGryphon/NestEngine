a56c77db5d135b687ff55b19b2cbf73e754329c5
SownGryphon <zhenyastratford@gmail.com>
2024-04-27 20:57:23 +1000

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -12,6 +12,7 @@ IncludeDir["Glad"] = "NestEngine/vendor/Glad/include"
 include "NestEngine/vendor/CHCL/CHCL"
 include "NestEngine/vendor/GLFW"
 include "NestEngine/vendor/Glad"
+include "NestEngine"
 
 project "NestGLSLEmbedder"
 	location "NestGLSLEmbedder"
@@ -27,64 +28,6 @@ project "NestGLSLEmbedder"
 	{
 		"%{prj.name}/src/*.cpp"
 	}
-
-project "NestEngine"
-	location "NestEngine"
-	kind "StaticLib"
-	language "C++"
-
-	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
-
-	files
-	{
-		"%{prj.name}/src/**.h",
-		"%{prj.name}/src/**.cpp",
-		"%{prj.name}/res/shaders/**.glsl*",
-		"%{prj.name}/res/embeddedShaders/**.h"
-	}
-
-	includedirs
-	{
-		"%{prj.name}/src",
-		"%{prj.name}/res",
-		"%{IncludeDir.CHCL}",
-		"%{IncludeDir.GLFW}",
-		"%{IncludeDir.Glad}"
-	}
-
-	links
-	{
-		"CHCL",
-		"GLFW",
-		"Glad",
-		"opengl32.lib"
-	}
-
-	dependson("NestGLSLEmbedder")
-
-	defines
-	{
-		"GLFW_INCLUDE_NONE"
-	}
-
-	filter "system:windows"
-		cppdialect "C++20"
-		staticruntime "On"
-		systemversion "latest"
-
-		defines
-		{
-			"NE_PLATFORM_WINDOWS"
-		}
-
-	filter "configurations:Debug"
-		defines { "NE_DEBUG", "NE_ENABLE_ASSERTS" }
-		symbols "On"
-
-	filter "configurations:Release"
-		defines "NE_RELEASE"
-		optimize "On"
 
 project "Sandbox"
 	location "Sandbox"
